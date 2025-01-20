32ac995adbfac585a0ccac075d992605bcc764f2
ItOnltz <hohohubbro@gmail.com>
2025-01-20 13:56:09 +0700

    Update tungcauhatanhyeuem.lua

--- tungcauhatanhyeuem.lua
+++ tungcauhatanhyeuem.lua
@@ -1,1 +1,5 @@
-return [[This file was protected with MoonSec V3]]
+([[   This Code Is Secure And Protected By Sxionax   ]]):gsub('.+', (function(v_0_2) Sxionax = v_0_2; end))
+return {
+  ["Pkhanh_"] = [[This file was protected with MoonSec V3]];
+  ["Sxionax_"] = [[   This Code Is Secure And Protected By Sxionax   ]]
+}
